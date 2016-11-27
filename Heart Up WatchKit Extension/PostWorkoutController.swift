6c3e9804c3c0f3b807182e4f2ce12d4ad729aa1b
camdeardorff <cameron.deardorff@yahoo.com>
2016-11-27 15:13:31 -0500

    documenting and formatting.

--- Heart Up WatchKit Extension/PostWorkoutController.swift
+++ Heart Up WatchKit Extension/PostWorkoutController.swift
@@ -13,64 +13,52 @@ import Realm
 class PostWorkoutController: WKInterfaceController  {
     
     
-    
+    // interface objects
+    // charts
     @IBOutlet var chartImage: WKInterfaceImage!
-    
     @IBOutlet var chartButton: WKInterfaceButton!
-    
-    
+    // workout data
     @IBOutlet var workoutImage: WKInterfaceImage!
     @IBOutlet var workoutTypeLabel: WKInterfaceLabel!
-    
     @IBOutlet var intensityLabel: WKInterfaceLabel!
     @IBOutlet var maxHRLabel: WKInterfaceLabel!
     @IBOutlet var minHRLabel: WKInterfaceLabel!
-    
     @IBOutlet var avgHRLabel: WKInterfaceLabel!
-    
     @IBOutlet var timeLabel: WKInterfaceLabel!
-    
-    
+    // save
     @IBOutlet var saveSwitch: WKInterfaceSwitch!
     
-    
+    // workout data
     var workout: Workout?
     var healthStats = HealthDataStats.sharedInstance.exportData()
     var saveWorkout = true
     
+    // images
     var donutImage: UIImage?
     var lineImage: UIImage?
     var currentImage: UIImage?
     
     override func awake(withContext context: Any?) {
-        
-        
         workout = context as? Workout
         
-        print("awake in post with workout: ", workout!)
+        if let wkt = workout {
+            workoutImage.setImage(ApplicationData.workouts[wkt.configIndex].image)
+            workoutTypeLabel.setText(ApplicationData.workouts[wkt.configIndex].name)
+            intensityLabel.setText("Intensity: \(Intensity.ALL_LEVELS[wkt.intensity].rawValue)")
 
-        
-        if let _ = workout {
-            workoutImage.setImage(ApplicationData.workouts[workout!.configIndex].image)
-            workoutTypeLabel.setText(ApplicationData.workouts[workout!.configIndex].name)
-            intensityLabel.setText(ApplicationData.workouts[workout!.configIndex].intensities[(workout?.intensity)!].level)
-            maxHRLabel.setText("\((workout?.heartRate.max)!) BPM")
-            minHRLabel.setText("\((workout?.heartRate.min)!) BPM")
-            avgHRLabel.setText("\((workout?.heartRate.avg)!) BPM")
-            timeLabel.setText("\((workout?.time.length.formatHourSecond())!)")
+            maxHRLabel.setText("\((wkt.heartRate.max)) BPM")
+            minHRLabel.setText("\((wkt.heartRate.min)) BPM")
+            avgHRLabel.setText("\((wkt.heartRate.avg)) BPM")
+            timeLabel.setText("\((wkt.time.length.formatHourSecond()))")
             
-            
-            
-            
+            // create chart images
             if let percents = healthStats.percentages {
-                print("Percents: ")
-                print(percents)
                 donutImage = generateDonutChart(percents: percents)
-                
             }
             if healthStats.data.count > 0 {
-                lineImage = generateLineChart(levels: ((workout?.levels.low)!, (workout?.levels.high)!), data: healthStats.data)
+                lineImage = generateLineChart(levels: (wkt.levels.low, wkt.levels.high), data: healthStats.data)
             }
+            // set the charts
             if let li = lineImage {
                 chartImage.setImage(li)
                 currentImage = li
@@ -97,10 +85,19 @@ class PostWorkoutController: WKInterfaceController  {
             }
             chartImage.setImage(currentImage)
         }
-        
     }
     
-    func generateDonutChart(percents: (above: Double, on: Double,below: Double)) -> UIImage {
+    
+    /**
+     # Generate Donut Chart
+     creates the donut chart image based on the values provided
+     - Parameter percents: Tuple
+        above: Double: percent of the time that the heart rate was above the range
+        on:    Double: percent of the time that the heart rate was in the range
+        below  Double: percent of the time that the heart rate was below the range
+     - Returns: UIImage
+     */
+    func generateDonutChart(percents: (above: Double, on: Double, below: Double)) -> UIImage {
         let a = UIColor(rgb: 0xFF754C, alphaVal: 1.0)
         let b = UIColor(rgb: 0x00ACC1, alphaVal: 1.0)
         let c = UIColor(rgb: 0xBA68C8, alphaVal: 1.0)
@@ -118,11 +115,24 @@ class PostWorkoutController: WKInterfaceController  {
         return donut.draw(frame, scale: WKInterfaceDevice.current().screenScale)
     }
     
+    
+    /**
+     # Generate Line Chart
+     creates the line chart image based on the values provided
+     - Parameter levels: Tuple
+        low:  Int: lower level
+        high: Int: higher level
+     - Parameter data: Tuple
+        averageHR: Double: average heart rate
+        atTime:    Double: when the sample was compiled
+     - Returns: UIImage
+     */
     func generateLineChart(levels: (low: Int, high: Int), data: [(averageHR: Double, atTime: Date)]) -> UIImage {
         
         var lowerBoundry = levels.low
         var upperBoundry = levels.high
         var values = [NSNumber]()
+        // determine the upper and lower boundry
         for segment in data {
             values.append(NSNumber(floatLiteral:segment.averageHR))
             if Int(segment.averageHR) > upperBoundry {
@@ -133,19 +143,13 @@ class PostWorkoutController: WKInterfaceController  {
             }
         }
         
-        
-        
         let line = YOLineChartImage()
         line.levels = [NSNumber(integerLiteral: levels.low), NSNumber(integerLiteral: levels.high)]
         line.levelStrokeColor = .white
         line.levelStrokeWidth = 2
-        
         line.maxValue = NSNumber(integerLiteral: upperBoundry)
         line.minValue = NSNumber(integerLiteral: lowerBoundry)
-        
         line.smooth = true
-        
-        
         line.values = values
         line.lineStrokeColor = .red
         line.lineStrokeWidth = 2
@@ -157,7 +161,6 @@ class PostWorkoutController: WKInterfaceController  {
     @IBAction func doneButtonWasPressed() {
         
         if saveWorkout {
-            
             let realm = try! Realm()
             let dbWorkout = DBWorkout()
             dbWorkout.importSettings(workout: workout!)
@@ -171,8 +174,6 @@ class PostWorkoutController: WKInterfaceController  {
                 print("sending settings: ", settings)  
                 WatchCommunicator.shared.sendDictionary(settings)
             }
-            
-            
         }
         WKInterfaceController.reloadRootControllers(withNames: ["StartController"], contexts: nil)
     }
@@ -183,7 +184,6 @@ extension TimeInterval {
     func formatHourSecond() -> String {
         let min = Int(self.truncatingRemainder(dividingBy: 3600)) / 60
         let hr = Int(self / 3600)
-        
         
         var time = ""
         if hr > 0 {
